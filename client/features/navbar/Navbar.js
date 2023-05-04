9d084b07945bb4283811a99ddcc125f309d44d24
Your Nameexit <sjmichaels95@gmail.com>
2023-05-04 15:25:30 -0500

    trying to link tailwindcss with our stylesheet

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -9,24 +9,43 @@ const Navbar = () => {
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <div>
-      <h1>FS-App-Template</h1>
       <nav>
         {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
+            <img className="logo" src="https://ia.media-imdb.com/images/M/MV5BODc4MTA3NjkzNl5BMl5BcG5nXkFtZTgwMDg0MzQ2OTE@._V1_.png" />
+            <div class="dropdown">
+              <button class="dropbtn">Dropdown</button>
+              <div class="dropdown-content">
+                <a href="#">Movies</a>
+                <a href="#">TV Shows</a>
+                <a href="#">Awards & Events</a>
+              </div>
+            </div>
+            <input type="text" placeholder="Search.."></input>
             <button type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
           </div>
         ) : (
-          <div>
+          <div id="nav-container">
             {/* The navbar will show these links before you log in */}
+            <img className="logo" src="https://ia.media-imdb.com/images/M/MV5BODc4MTA3NjkzNl5BMl5BcG5nXkFtZTgwMDg0MzQ2OTE@._V1_.png" />
+            <div class="dropdown">
+              <button class="dropbtn">Dropdown</button>
+              <div class="dropdown-content">
+              <a href="#">Movies</a>
+                <a href="#">TV Shows</a>
+                <a href="#">Awards & Events</a>
+              </div>
+            </div>
+            <input className="search" type="text" placeholder="Search.."></input>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
           </div>
@@ -36,5 +55,4 @@ const Navbar = () => {
     </div>
   );
 };
-
 export default Navbar;
