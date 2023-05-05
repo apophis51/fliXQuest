6565b98e482e33c43600c9e5a41e1b584fc1d98f
Your Nameexit <sjmichaels95@gmail.com>
2023-05-05 11:08:09 -0500

    updated navbar to return functionality without tailwind styles

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../app/store";
-
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
@@ -11,142 +10,56 @@ const Navbar = () => {
     dispatch(logout());
     navigate("/login");
   };
-
   return (
     <div>
       <nav>
         {isLoggedIn ? (
-          <div>
+          <div className="container">
+            <div className="img">
             {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <img className="logo" src="https://ia.media-imdb.com/images/M/MV5BODc4MTA3NjkzNl5BMl5BcG5nXkFtZTgwMDg0MzQ2OTE@._V1_.png" />
+            <img
+              className="logo"
+              src="https://ia.media-imdb.com/images/M/MV5BODc4MTA3NjkzNl5BMl5BcG5nXkFtZTgwMDg0MzQ2OTE@._V1_.png"
+            />
+            </div>
             <div className="dropdown">
-              <button className="">Dropdown</button>
+              <button className="dropbtn">Dropdown</button>
               <div className="dropdown-content">
-                <a href="#">Movies</a>
-                <a href="#">TV Shows</a>
-                <a href="#">Awards & Events</a>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
               </div>
             </div>
-            <input type="text" placeholder="Search.."></input>
+            <input type="text" placeholder="Search.." />
             <button type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
           </div>
+
         ) : (
-          <div id="nav-container" >
+          <div className="container">
+            <div className="img">
             {/* The navbar will show these links before you log in */}
-            <img className="logo" src="https://ia.media-imdb.com/images/M/MV5BODc4MTA3NjkzNl5BMl5BcG5nXkFtZTgwMDg0MzQ2OTE@._V1_.png" />
-            <div className="dropdown bg-black">    
-              <button className="bg-black"  >Dropdown</button>
-              <div className="dropdown-content">
-              <a href="#" className="text-gray-700">Movies</a>
-                <a href="#">TV Shows</a>
-                <a href="#">Awards & Events</a>
-              </div>
-              <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
-
-<div className="rating">
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-</div>
-
-
-<footer className="footer p-10 bg-base-200 text-base-content">
-  <div>
-    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
-  </div> 
-  <div>
-    <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
-  </div> 
-  <div>
-    <span className="footer-title">Company</span> 
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
-  </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
-    <a className="link link-hover">Cookie policy</a>
-  </div>
-</footer>
-
-<div className="text-sm breadcrumbs">
-  <ul>
-    <li><a>Home</a></li> 
-    <li><a>Documents</a></li> 
-    <li>Add Document</li>
-  </ul>
-</div>
-
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-
-
-<div className="flex  w-full">
-  <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div className="divider"></div> 
-  <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
-
-
-
-
-
-
-
-
-<button className="btn gap-2">
-  Inbox
-  <div className="badge">+99</div>
-</button>
-<button className="btn gap-2">
-  Inbox
-  <div className="badge badge-secondary">+99</div>
-</button>
-
-
-
-
-
+            <img
+              className="logo"
+              src="https://ia.media-imdb.com/images/M/MV5BODc4MTA3NjkzNl5BMl5BcG5nXkFtZTgwMDg0MzQ2OTE@._V1_.png"
+            />
             </div>
-            <input className="search bg-black box-border caro" type="text" placeholder="Search.."></input>
-            <Link to="/login" className="text-blue-600 btn" >Login</Link>
+            <div className="dropdown">
+              <button className="dropbtn">Dropdown</button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+            <input type="text" placeholder="Search.." />
+            <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
-            <div className="card card-title"></div>
           </div>
         )}
       </nav>
       <hr />
-      <button className="btn btn-secondary">hfffi</button>
-
     </div>
 
   );
