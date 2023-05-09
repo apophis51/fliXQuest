27912b4087627a6f136bcb464979bd0a22676828
Your Nameexit <sjmichaels95@gmail.com>
2023-05-09 17:07:49 -0500

    broken carousel. starting to implement working functionality

--- client/features/Movies/AllMovies.js
+++ client/features/Movies/AllMovies.js
@@ -9,6 +9,10 @@ const AllMovies = () => {
   const status = useSelector((state) => state.AllMovies.status);
   const error = useSelector((state) => state.AllMovies.error);
 
+  function numberClicker() {
+    num2++
+  }
+
   useEffect(() => {
     dispatch(fetchAllMovies());
   }, [dispatch]);
@@ -31,47 +35,43 @@ const AllMovies = () => {
   if (!Array.isArray(movies) || movies.length === 0) {
     return <div>No movies found.</div>;
   }
-
+  let num = 0;
+  let num2 = 5;
   return (
     <div className="movies-container">
       <p className="page-title">All Movies</p>
-      <div className="categories">
-        {/* <p>Title</p> */}
-        {/* <p className="text">Ranking</p> */}
-        <p id="release-date" className="text">Release Date</p>
-      </div>
+      <div className="categories"></div>
       <div className="AllMovies">
-        <div className="inner-container">
+        <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+            <a href="#item1" className="btn btn-circle">❮</a>
+            </div>
+          </div>
+          </div>
+          <div className="carousel rounded-box">
           {movies.map((movie) => (
-            <div className="movies" key={movie.id}>
+            <div id={"item" + num++} className="carousel-item " key={movie.id}>
               <Link to={`/movies/${movie.id}`}>
                 <div className="inner-box">
-                <div className="show-name">
-                  <img
-                    className="movie-poster"
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <div className="name">{movie.title}</div>
+                  <div className="show-name">
+                    <img
+                      className="movie-poster"
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                    <div className="name">{movie.title}</div>
                   </div>
                   <div className="rank-star">
-                  <img
-                        className="star"
-                        src="https://www.supercoloring.com/sites/default/files/styles/drawing_full/public/fif/2017/05/gold-star-paper-craft.png"
-                      ></img>
-                    <p className="text">
-                      {movie.vote_average}
-                    </p>
-                    </div>
-                    <p className="text">{movie.release_date}</p>
-
+                  </div>
                 </div>
               </Link>
             </div>
           ))}
         </div>
-      </div>
-    </div>
+        <div id="slide1" className="carousel-item relative w-full">
+            <a href={"#item" + num2} onClick={numberClicker()} className="btn btn-circle">❯</a>
+            </div>
+        </div>
   );
 };
 
