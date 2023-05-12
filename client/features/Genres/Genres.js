c5cdc758b585507252e08eb912a86d96835514b8
Rhyan St. Clair <rhyan9311@gmail.com>
2023-05-12 16:12:41 -0400

    added a handle movie click

--- client/features/Genres/Genres.js
+++ client/features/Genres/Genres.js
@@ -12,6 +12,7 @@ function Genres() {
   // added
   const [currentItemNumber, setCurrentItemNumber] = useState(1);
   const [numOfItems, setNumOfItems] = useState(0);
+   const [videoData, setVideoData] = useState({});
   const tagsEl = useRef(null);
 
   useEffect(() => {
@@ -75,6 +76,17 @@ function Genres() {
   };
   // added
   let num2 = 3;
+
+  const handleMovieClick = (movie) => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movie.id}/videos?$api_key=1cf50e6248dc270629e802686245c2c8`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setVideoData(data);
+        // setIsOpen(true);
+      });
+  };
 
   function renderMovies() {
     const filteredMovies =
@@ -146,111 +158,3 @@ function Genres() {
 }
 
 export default Genres;
-
-// First Version that works with genres, overview, and single movie / no carousel
-
-// return filteredMovies.map((movie) => (
-//       <div key={movie.id} className="movie">
-//         {/* added */}
-//         <div key={movie.id} className="carousel-item inline-flex">
-//           <Link to={`/movies/${movie.id}`}>
-//             <div className="inner-box">
-//               <div className="show-name">
-//                 <img
-//                   className="movie-poster"
-//                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-//                   alt={movie.title}
-//                 />
-//                 {/* <div className="name">{movie.title}</div> */}
-//               </div>
-//               <div className="rank-star"></div>
-//             </div>
-//           </Link>
-//         </div>
-//         {/* <img
-//           src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-//           alt={movie.title}
-//         /> */}
-//         <div className="movie-info">
-//           <h3>{movie.title}</h3>
-//           <span className="vote-average">{movie.vote_average}</span>
-//         </div>
-//         <div className="overview">
-//           <h3>Overview:</h3>
-//           {movie.overview}
-//         </div>
-//       </div>
-//     ));
-//   }
-
-{
-  /* <div className="movie-info">
-          <h3>{movie.title}</h3>
-          <span className="vote-average">{movie.vote_average}</span>
-        </div>
-        <div className="overview">
-          <h3>Overview:</h3>
-          {movie.overview}
-        </div> */
-}
-
-// return (
-//       <div className="movies-container">
-//         <p className="page-title">All Movies</p>
-//         <div className="categories"></div>
-//         <div className="AllMovies">
-//           <div className="carousel-container">
-//             <div className="carousel-items"></div>
-//           </div>
-//         </div>
-//         <div className="carousel rounded-box">
-//           <div className="absolute flex justify-between transform -translate-y-1/2 left-12 right-12 top-1/3 text-white">
-//             <a
-//               href={"#item" + currentItemNumber}
-//               className="carousel-item"
-//               onClick={() => handleItemDelete()}
-//             >
-//               ❮
-//             </a>
-//             <a
-//               href={"#item" + currentItemNumber}
-//               className="carousel-item"
-//               onClick={() => handleItemClick()}
-//             >
-//               ❯
-//             </a>
-//           </div>
-//           {filteredMovies.map((movie) => (
-//             <div
-//               className="carousel-item inline-flex"
-//               id={"item" + num++}
-//               key={movie.id}
-//             >
-//               <Link to={`/movies/${movie.id}`}>
-//                 <div className="inner-box">
-//                   <div className="show-name">
-//                     <img
-//                       className="movie-poster"
-//                       src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-//                       alt={movie.title}
-//                     />
-//                   </div>
-//                   <div className="movie-info">
-//                     <h3>{movie.title}</h3>
-//                     <span className="vote-average">{movie.vote_average}</span>
-//                   </div>
-//                   <div className="overview">
-//                     <h3>Overview:</h3>
-//                     {movie.overview}
-//                   </div>
-//                 </div>
-//               </Link>
-//             </div>
-//           ))}
-//         </div>
-//         <div id="slide1" className="carousel-item relative w-full">
-//           \{" "}
-//         </div>
-//       </div>
-//     );
-//   }
