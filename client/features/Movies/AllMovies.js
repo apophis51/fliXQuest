5c1b24a18f25ee2d49beb7ce696a98250000c13f
apophis51 <malcolmxvernon@hotmail.com>
2023-05-22 04:04:56 -0400

    Css Edits

--- client/features/Movies/AllMovies.js
+++ client/features/Movies/AllMovies.js
@@ -84,12 +84,12 @@ const AllMovies = () => {
             key={movie.id}
           >
             <Link to={`/movies/${movie.id}`}>
-              <div key={movie.id} className="movie">
+              <div key={movie.id} className="movie text-white">
                 <img
                   src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
                   alt={movie.title}
                 />
-                <div className="movie-info">
+                <div className="movie-info b">
                   <h3>{movie.title}</h3>
                   <span className="vote-average">{movie.vote_average}</span>
                 </div>
--- client/features/TVSHOWS/AllTVshows.js
+++ client/features/TVSHOWS/AllTVshows.js
@@ -84,9 +84,9 @@ const AllTVshows = () => {
                   <img
                     className="movie-poster"
                     src={`https://image.tmdb.org/t/p/w500${tvshow.poster_path}`}
-                    alt={tvshow.title}
+                    alt={tvshow.name}
                   />
-                  <div className="name">{tvshow.title}</div>
+                  <div className="name">{tvshow.name}</div>
                 </div>
                 <div className="rank-star"></div>
               </div>
